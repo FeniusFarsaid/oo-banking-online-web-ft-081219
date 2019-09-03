eaf397be1aadff9b10ed44fc84ea8007f2783ca1
David Ingram <david@ingramaviation.com>
2019-09-03 21:44:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,8 @@ class Transfer
 
   def reverse_transfer
     if @status == "complete"
-      @receiver.balance -= @amount 
-       @sender.balance += @amount
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
       @status = "reversed"
     end
   end
